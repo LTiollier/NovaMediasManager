6e1dfd460cc0cda917bff972de6b4ca0f93de244
tiollier <leo@web-id.fr>
2020-06-13 17:24:46 +0200

    current change

---
+++ bootstrap/cache/packages.php
@@ -0,0 +1,80 @@
+<?php return array (
+  'beyondcode/laravel-dump-server' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'BeyondCode\\DumpServer\\DumpServerServiceProvider',
+    ),
+  ),
+  'fideloper/proxy' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Fideloper\\Proxy\\TrustedProxyServiceProvider',
+    ),
+  ),
+  'intervention/image' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Intervention\\Image\\ImageServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Image' => 'Intervention\\Image\\Facades\\Image',
+    ),
+  ),
+  'jeroen-g/laravel-packager' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'JeroenG\\Packager\\PackagerServiceProvider',
+    ),
+  ),
+  'laravel/nova' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Nova\\NovaCoreServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Nova' => 'Laravel\\Nova\\Nova',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'nesbot/carbon' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Carbon\\Laravel\\ServiceProvider',
+    ),
+  ),
+  'nunomaduro/collision' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
+    ),
+  ),
+  'shipping-docker/vessel' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Vessel\\VesselServiceProvider',
+    ),
+  ),
+  'spatie/laravel-medialibrary' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Spatie\\MediaLibrary\\MediaLibraryServiceProvider',
+    ),
+  ),
+);
